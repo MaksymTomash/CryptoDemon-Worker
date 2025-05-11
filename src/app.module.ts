6b928da328ec7fbec476896455060aae095367f2
MaksymTomash <maks.tomash4588@gmail.com>
2025-05-12 01:29:09 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { UserCurrencyModule } from './user-currency/user-currency.module';
         type: 'postgres',
         url: process.env.DATABASE_URL,
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: false,
         ssl: true,
         extra: {
           ssl: {
